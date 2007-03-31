4f1dd05885371e05bb88d9bff0183ff6f5ce29a1
dean.edwards <dean.edwards@ff4224f9-ee2c-0410-8f92-13d0cef0be7b>
2007-03-31 01:37:11 +0000

    Google test.

---
+++ test.js
@@ -0,0 +1,1 @@
+alert('This browser supports JavaScript served with a mime-type of "text/plain".\nThis message has been served gzipped from Google\'s servers.\n\nGood enough for your JavaScript library?');
